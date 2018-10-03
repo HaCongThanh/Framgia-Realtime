5045955414459ff549b2193b2d2f3ce3378e3abc
HaCongThanh <hacongthanh.it.hubt@gmail.com>
2018-10-03 15:43:01 +0700

    Mix and Frontend Ver01

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,12 @@ Route::get('/', function () {
     return view('coming-soon');
 });
 
+Route::group(['prefix' => 'dev'], function() {
+	Route::get('/', 'HomeController@index')->name('home');
+});
+
+Route::get('/realtime', 'SendMessageController@realTime')->name('realTime');
+
 Route::get('/send', 'SendMessageController@index')->name('send');
 
 Route::post('/postMessage', 'SendMessageController@sendMessage')->name('postMessage');
